727dc3f96e7107601e3d366be6d34a0c5dba4b42
YanhqZ <yanhongquan94@gmail.com>
2025-08-05 10:31:54 +0800

    Migrate example Gradle plugins with the declarative plugins block

--- example/android/app/src/main/kotlin/com/example/example_for_oktoast/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/example_for_oktoast/MainActivity.kt
@@ -2,6 +2,4 @@ package com.example.example_for_oktoast
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-  // The default activity that extends flutter's activity.
-}
+class MainActivity: FlutterActivity()
