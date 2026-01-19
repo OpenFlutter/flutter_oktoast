4aa3bbc2104e8ecb55c756b2b94f1ef19e5bb3e4
YanhqZ <yanhongquan94@gmail.com>
2026-01-19 17:20:20 +0800

    Revert "Migrate example Gradle plugins with the declarative plugins block"
    This reverts commit 727dc3f96e7107601e3d366be6d34a0c5dba4b42.

--- example/android/app/src/main/kotlin/com/example/example_for_oktoast/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/example_for_oktoast/MainActivity.kt
@@ -2,4 +2,6 @@ package com.example.example_for_oktoast
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+  // The default activity that extends flutter's activity.
+}
